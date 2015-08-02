150abafbaac1bb086414ad30426c4b6808ab183c
Riza Fahmi <rizafahmi@gmail.com>
2015-08-02 12:27:36 +0700

    Scraping the adwords using nokogiri

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,8 +1,19 @@
 class Keyword < ActiveRecord::Base
 
   def self.import(file)
+    require 'open-uri'
+    require 'nokogiri'
     CSV.foreach(file.path, headers: true) do |row|
-      Keyword.create! row.to_hash
+
+      data = row.to_hash
+      url = "https://www.google.com/search?q=#{data['keywords'].gsub(" ", "+")}"
+      doc = Nokogiri::HTML(open(url))
+
+      data['adwords_right'] = doc.css("#rhs_block").search("li.ads-ad").count
+      data['adwords_top'] = doc.css("#center_col").search("li.ads-ad").count
+      data['adwords_total'] = data['adwords_top'] + data['adwords_right']
+
+      Keyword.create! data
     end
   end
 end
