40a4c815640c3268a5f442ca6037146c26e1807a
Riza Fahmi <rizafahmi@gmail.com>
2015-08-02 22:37:27 +0700

    Finish authentication part

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'log_in' => 'sessions#new', :as => 'log_in'
+  get 'log_out' => 'sessions#destroy', :as => 'log_out'
   get 'sign_up' => 'users#new', :as => 'sign_up'
-  resources :users
 
+  resources :sessions
+  resources :users
   resources :keywords do
     collection { post :import }
   end
